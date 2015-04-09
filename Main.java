32e5dc925f526278bee8e036e120352a6e9597d4
LouLabourette <nikita.yegorov8@gmail.com>
2015-04-09 02:56:56 +0100

    Sum of mulitples of 3 and 5 in 1000

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main 
+ {
+  public static void main(String[] args)
+  {
+  SumOfMulitplesOfThreeAndFive test = new SumOfMulitplesOfThreeAndFive();
+  test.finalAnswer();
+  System.out.println("The Final Answer is: " + test.sumofmultfiveandthree);
+  }
+  
+
+ }
---
+++ SumOfMulitplesOfThreeAndFive.java
@@ -0,0 +1,28 @@
+public class SumOfMulitplesOfThreeAndFive
+{
+  //Задаём переменную, которая делится на 3, 5 их сумму (поля).
+  public int sumofmultfiveandthree = 0;
+  //Задаём функциi, которая вычислит все числа,которые делятся на 3 и 5 и их сумму (методi).
+  public void finalAnswer() 
+    
+      {
+    int i = 3; 
+    int range = 1000;
+    while(i < 1000) 
+        {
+         if(i % 3 == 0 | i % 5 == 0) 
+         {
+          this.sumofmultfiveandthree += i;
+          System.out.println(this.sumofmultfiveandthree);
+         }
+     i ++;
+   
+         }
+     
+    
+  
+      }
+  
+
+
+}
